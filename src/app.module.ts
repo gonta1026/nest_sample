bdc92f474e1e80aa93dd6f4db50ac48858ede384
gonta1026 <katano.original@gmail.com>
2023-09-08 00:00:49 +0900

    add: 諸々の修正と実装

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,18 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UserModule, TodoModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    UserModule,
+    TodoModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
